6b3a31900fbd75e1166c467561830a971ab47a6d
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-09 21:38:23 -0700

    Added a login page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import TextInput from "../common/TextInput";
+import PasswordInput from "../common/PasswordInput";
 
-const HomePage = () => (
-  <div className="jumbotron">
-    <h1>Admin Page</h1>
-    <p>Manage things on admin page</p>
-    <Link to="about" className="btn btn-primary btn-lg">
-      Learn More
-    </Link>
-  </div>
-);
+function HomePage(errors, onChange) {
+  return (
+    <div className="jumbotron">
+      <h1>User Admin Page</h1>
+      <p>Please login</p>
+
+      <TextInput
+        name="username"
+        label="Username"
+        onChange={onChange}
+        error={errors.username}
+      />
+
+      <PasswordInput
+        name="password"
+        label="Password"
+        onChange={onChange}
+        error={errors.password}
+      />
+
+      <Link to="users" className="btn btn-primary btn-lg">
+        Login
+      </Link>
+    </div>
+  );
+}
+
+HomePage.propTypes = {
+  errors: PropTypes.object,
+  onChange: PropTypes.func.isRequired
+};
 
 export default HomePage;
